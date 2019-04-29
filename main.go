07214c7b777955e90bb2b93a38ba86db0412fd17
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-04-29 09:20:27 +0100

    Add more sticker filters
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- main.go
+++ main.go
@@ -191,12 +191,44 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	includeStickers := r.URL.Query()["include-sticker"]
+	excludeStickers := r.URL.Query()["exclude-sticker"]
+
+	filteredCards := make(rubbernecker.Cards, 0)
+	for _, card := range cards {
+		shouldAdd := true
+
+		if len(includeStickers) > 0 {
+			shouldAdd = false
+			for _, sticker := range card.Stickers {
+				for _, includedStickerName := range includeStickers {
+					if sticker.Name == includedStickerName {
+						shouldAdd = true
+					}
+				}
+			}
+		} else if len(excludeStickers) > 0 {
+			shouldAdd = true
+			for _, sticker := range card.Stickers {
+				for _, excludedStickerName := range excludeStickers {
+					if sticker.Name == excludedStickerName {
+						shouldAdd = false
+					}
+				}
+			}
+		}
+
+		if shouldAdd {
+			filteredCards = append(filteredCards, card)
+		}
+	}
+
 	resp.
 		WithConfig(&rubbernecker.Config{
 			ReviewalLimit: 4,
 			ApprovalLimit: 5,
 		}).
-		WithCards(combineCards(cards, doneCards), false).
+		WithCards(combineCards(filteredCards, doneCards), false).
 		WithSampleCard(&rubbernecker.Card{}).
 		WithTeamMembers(members).
 		WithFreeTeamMembers().
